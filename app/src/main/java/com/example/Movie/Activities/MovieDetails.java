577a4ff76a1c11fc7f4c34ab4006704622673aeb
eslamlii <preslamali@hotmail.com>
2020-01-27 07:08:13 +0200

    update

--- app/src/main/java/com/example/Movie/Activities/MovieDetails.java
+++ app/src/main/java/com/example/Movie/Activities/MovieDetails.java
@@ -6,9 +6,20 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.Movie.Model.Movie;
 import com.example.Movie.R;
+import com.example.Movie.Util.Constants;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MovieDetails extends AppCompatActivity {
 
@@ -17,8 +28,10 @@ public class MovieDetails extends AppCompatActivity {
     private ImageView coverImage;
     private ImageView posterImage;
     private TextView movieYear;
+    private TextView runTime;
     private TextView director;
     private TextView overview;
+    private TextView tagline;
 
     private RequestQueue queue;
     private String movieId;
@@ -28,11 +41,48 @@ public class MovieDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
 
+        queue = Volley.newRequestQueue(this);
+
         movie = (Movie) getIntent().getSerializableExtra("movie");
         movieId = movie.getImdbId();
 
         setUpUI();
         getMoviesDetails(movieId);
+    }
+
+    private void getMoviesDetails(String id) {
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                Constants.URL_MOVIE + id + Constants.API, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    movieTitle.setText(response.getString("original_title"));
+                    movieYear.setText(response.getString("release_date"));
+                    runTime.setText(response.getString("runtime") + "mins");
+                    overview.setText(response.getString("overview"));
+                    tagline.setText(response.getString("tagline"));
+
+                    Picasso.get()
+                            .load("https://image.tmdb.org/t/p/w500" + response.getString("poster_path"))
+                            .into(posterImage);
+
+                    Picasso.get()
+                            .load("https://image.tmdb.org/t/p/w500" + response.getString("backdrop_path"))
+                            .into(coverImage);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("Error: " + error.getMessage());
+            }
+        });
+        queue.add(jsonObjectRequest);
     }
 
     private void setUpUI() {
@@ -42,5 +92,7 @@ public class MovieDetails extends AppCompatActivity {
         movieYear = findViewById(R.id.yearReleaseID);
         director = findViewById(R.id.directedID);
         overview = findViewById(R.id.overviewID);
+        runTime = findViewById(R.id.durationID);
+        tagline = findViewById(R.id.tagLine);
     }
 }
